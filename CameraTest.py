bfd7f3f08eb26bb13d1a2b1dd6cfc9cba6d0b072
SONG-YOUNG-WOONG <hero840@gmail.com>
2016-10-07 03:55:33 +0900

    added print hello

--- CameraTest.py
+++ CameraTest.py
@@ -2,9 +2,10 @@ import cv2
 
 camera = cv2.VideoCapture(0)
 
+print "hello"
+
 if camera.isOpened() == True:
     ret = camera.read()
     if ret[0] == True:
         cv2.imwrite("test.jpg", ret[1])
         camera.release()
-
