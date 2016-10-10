4fed8f70aabf59df8c3df1f9e45022ffa5e734dd
0bear <hero840@gmail.com>
2016-10-11 00:34:23 +0900

    added comments for debugging

--- Videocapture.py
+++ Videocapture.py
@@ -8,17 +8,21 @@ camera = cv2.VideoCapture(0)
 out = cv2.VideoWriter('Output.mp4', -1, 15.0, (1024,768))
 
 while(camera.isOpened):
+        print 'camera is opened'
         ret, frame = camera.read()
+        print 'camera read'
         if ret == True:
             frame = cv2.flip(frame,0)
             out.write(frame)
+            print 'frame wrote'
 
             # cv2.imshow('frame',frame)
-            if cv2.waitKey(1) & 0xFF ==ord('q'):
+            if cv2.WaitKey(1) & 0xFF == ord('q'):
                 break
 
         else:
             break
-            camera.release()
-            out.release()
-            cv2.destroyAllWindows()
+
+camera.release()
+out.release()
+cv2.destroyAllWindows()
