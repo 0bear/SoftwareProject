f7f3b1b5c0b21e55803e0470e02610efc5208ba9
0bear <hero840@gmail.com>
2016-10-11 01:42:56 +0900

    .avi -1

--- Videocaptureexample.py
+++ Videocaptureexample.py
@@ -4,10 +4,10 @@ import cv2
 cap = cv2.VideoCapture(0)
 
 # Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+# fourcc = cv2.VideoWriter_fourcc(*'DIVX')
 
 #out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
-out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (640,480))
+out = cv2.VideoWriter('output.avi', -1, 20.0, (640,480))
 
 while(cap.isOpened()):
     ret, frame = cap.read()
