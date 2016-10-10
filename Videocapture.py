f8645ed6ac3fdd1af13a00d2f1062ce15780bca5
0bear <hero840@gmail.com>
2016-10-11 00:36:09 +0900

    added repeat() and changed waitkey

--- Videocapture.py
+++ Videocapture.py
@@ -17,7 +17,8 @@ while(camera.isOpened):
             print 'frame wrote'
 
             # cv2.imshow('frame',frame)
-            if cv2.WaitKey(1) & 0xFF == ord('q'):
+            repeat()
+            if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
         else:
